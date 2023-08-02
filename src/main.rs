1b2cc789ed62adaa6b5d1c1966ce3f54a041c980
ken.huang <ken.huang@104.com.tw>
2023-08-02 17:26:10 +0800

    result and ?

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 /*
+Result<T> 是一个枚举类型用于描述返回的结果或错误，它包含两个成员(变体 variants) :
 
-Rust 中最简单的错误处理方式就是使用 panic。它会打印出一条错误信息并打印出栈调用情况，最终结束当前线程:
-
-若 panic 发生在 main 线程，那程序会随之退出
-如果是在生成的( spawn )子线程中发生 panic, 那么当前的线程会结束，但是程序依然会继续运行
+Ok(T): 返回一个结果值 T
+Err(e): 返回一个错误，e 是具体的错误值
+简而言之，如果期待一个正确的结果，就返回 Ok，反之则是 Err。
 */
-// 填空
-fn drink(beverage: &str) {
-    if beverage == "lemonade" {
-        println!("Success!");
-        // 实现下面的代码
-        __
-    }
 
-    println!("Exercise Failed if printing out this line!");
+// 填空并修复错误
+use std::num::ParseIntError;
+
+fn multiply(n1_str: &str, n2_str: &str) -> __ {
+    let n1 = n1_str.parse::<i32>();
+    let n2 = n2_str.parse::<i32>();
+    Ok(n1.unwrap() * n2.unwrap())
 }
 
 fn main() {
-    drink(__);
+    let result = multiply("10", "2");
+    assert_eq!(result, __);
 
-    println!("Exercise Failed if printing out this line!");
+    let result = multiply("t", "2");
+    assert_eq!(result.__, 8);
+
+    println!("Success!")
 }
