8f2f753881ec3affd006fc30a034512d53e2ef13
ken.huang <ken.huang@104.com.tw>
2023-08-02 17:33:42 +0800

    result and ? 1

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ Err(e): 返回一个错误，e 是具体的错误值
 // 填空并修复错误
 use std::num::ParseIntError;
 
-fn multiply(n1_str: &str, n2_str: &str) -> __ {
+fn multiply(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError> {
     let n1 = n1_str.parse::<i32>();
     let n2 = n2_str.parse::<i32>();
     Ok(n1.unwrap() * n2.unwrap())
@@ -17,10 +17,10 @@ fn multiply(n1_str: &str, n2_str: &str) -> __ {
 
 fn main() {
     let result = multiply("10", "2");
-    assert_eq!(result, __);
+    assert_eq!(result, Ok(20));
 
-    let result = multiply("t", "2");
-    assert_eq!(result.__, 8);
+    let result = multiply("4", "2");
+    assert_eq!(result.unwrap(), 8);
 
     println!("Success!")
 }
